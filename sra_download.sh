c7e10eaf793e89cebd82ea3e18422068bc24f811
Sonja Lecic <slecic8@gmail.com>
2020-11-04 12:00:26 +0100

    Update sra_download.sh

--- sra_download.sh
+++ sra_download.sh
@@ -4,6 +4,6 @@ for file in $(<SRR_Acc_List-6.txt)
 do
 
      date
-     prefetch SRR23${file} && vdb-validate SRR23${file} && fastq-dump -split-files SRR23${file} -gzip -O Fastq_florida
+     prefetch SRR23${file} && vdb-validate SRR23${file} && fastq-dump --split-files SRR23${file} --gzip -O Fastq_florida
      date
 done
